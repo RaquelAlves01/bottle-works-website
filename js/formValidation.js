3fc447e1ebaeec087532a063a0d54ae19cd79916
guer0157 <32390693+guer0157@users.noreply.github.com>
2019-01-28 08:18:34 -0500

    u
    h

--- js/formValidation.js
+++ js/formValidation.js
@@ -30,39 +30,39 @@ let init={
     validation: function(ev){
         ev.preventDefault()
         //required
+        val_count=[]
         let nameField=document.getElementById('name')
         nameField.onkeypress=function(){
             nameField.style.borderColor="#666";
-            // let errName=nameField.nextElementSibling;
-            // errName.classList.remove("error-message");
-            // errName.classList.add("hide");
         }
         if (nameField.value == "") {
             nameField.style.borderColor="red";
+            val_count.push(1)
         }
         // //required (address)
         let addressField=document.getElementById('position')
         addressField.onkeypress=function(){
-            addressField.style.borderColor="#666";
-            // let errName=nameField.nextElementSibling;
-            // errName.classList.remove("error-message");
-            // errName.classList.add("hide");
+            addressField.style.borderColor="black";
+
         }
         if (addressField.value == "") {
             addressField.style.borderColor="red";
+            val_count.push(2)
         }
         // //required
         let emailField=document.getElementById('email')
         emailField.onkeypress=function(){
-            emailField.style.borderColor="#666";
+            emailField.style.borderColor="black";
          }
          let emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
          let emailPass = emailPattern.test(emailField.value);
          if (emailPass == false) {
              if (emailField.value.length == 0) {
                  emailField.style.borderColor="red";
+                 val_count.push(3)
              } else {
                  emailField.style.borderColor="red";
+                 val_count.push(3)
              }
          }
       let phoneField=document.getElementById('phone')
@@ -85,28 +85,25 @@ let init={
         // //consent-yes
         // //consent-no
         let publishBoolean=document.querySelectorAll('[name=consent]:checked')
+        let publishBooleanValue
         if(publishBoolean.length===0){
             let validatePublishBoolean=document.querySelectorAll('label.milli.yes')
             validatePublishBoolean.forEach(radio=>{
                 radio.addEventListener('click',()=>{
                     validatePublishBoolean.forEach(r=>r.style.color='black')
                 })
-                radio.style.color="red"})
+                radio.style.color="red"
+                
+            })
+            val_count.push(5)
             console.log(validatePublishBoolean);
+        }else{
+            publishBooleanValue=publishBoolean[0].attributes.id.value
         }
 
         // //required
         let taxEmailField=document.getElementById('tax-email')
   
-        //collection days
-        // let collectDaysArray=document.querySelectorAll('[name=collection-days]')
-        // let daysChecked=[]
-        // collectDaysArray.forEach(dayChecked=>{
-        //     if(dayChecked.checked){
-        //     daysChecked.push(dayChecked.id)
-        //     }
-        // })
-
         // //date
         let checkbox_days = document.querySelectorAll("input[name='collection-days']:checked");
         let checked_days=[]
@@ -124,6 +121,7 @@ let init={
                 })
                 
             }) 
+            val_count.push(6)
         }else{
             checkbox_days.forEach((dayClicked)=>{
              
@@ -134,51 +132,62 @@ let init={
         
         let startDateField=document.getElementById('start-date')
         startDateField.addEventListener('click',()=>{ startDateField.style.borderColor="black";})
-            // let errName=nameField.nextElementSibling;
-            // errName.classList.remove("error-message");
-            // errName.classList.add("hide");
         
         if (startDateField.value == "") {
             startDateField.style.borderColor="red";
+            val_count.push(7)
         }
- 
-        // //text
-        // let otherHoursField=document.getElementById('other-hours')
-  
-        // //Frequency get all input>type='radio' child elements
-        // let frequencyArray=document.querySelectorAll('[name=collection-frequency]')
-        
-      
-        //     let frequencyValue=null
-        //     frequencyArray.forEach((button)=>
-        //     {
-        //         if(button.checked){
-        //           frequencyValue=button.id
-        //     }               
-        //     })
-    
-        // //required yes : no type:radio
-        // let storageBoolean=document.querySelectorAll('[name=litre-storage]')
-     
-        // let storageValue=null
-        // storageBoolean.forEach(option=>{
-             
-        //     if(option.checked){
-        //         let regEx= new RegExp(/yes/);
-        //         let testReg=regEx.test(option.id)?storageValue='yes':storageValue='no'
-        //     }
-        //     })
+        let frequencyArray=document.querySelectorAll('[name=collection-frequency]:checked')
+        let frequencyValue
+        let collectionFreq=document.querySelectorAll(".collection-freq")
+        collectionFreq.forEach(b=>{
+           
+                b.addEventListener('click',()=>{
+                  collectionFreq.forEach(cf=>cf.style.color="black")  
+                })
+            b.style.color="black";
+        })
+        if(frequencyArray.length===0){
+            let collecting=document.querySelectorAll(".collection-freq")
+        collecting.forEach(b=>{
+            b.style.color="red";
+           
+        }) 
+        val_count.push(8)
+        }else{
+         frequencyValue=frequencyArray[0].attributes.id.value
+        }
+        let otherHoursField=document.getElementById("other-hours")
 
-        // //text
-        // let additionalInforField=document.getElementById('additional-info')
-        
-        // let responsesArray=[nameField.value,addressField.value,emailField.value,phoneField.value,publishBooleanValue,taxEmailField.value,daysChecked,startDateField.value,otherHoursField.value,frequencyValue,storageValue,additionalInforField.value];
-        // console.log("Reaching this line")
-        // sendToDb(responsesArray)
-        // ev.preventDefault();
-        
+        let storageBoolean=document.querySelectorAll('[name=litre-storage]:checked')
+        let storageValue;
+        if(storageBoolean.length===0){
+            let validatestorageBoolean=document.querySelectorAll('.storage')
+            validatestorageBoolean.forEach(radio=>{
+                radio.addEventListener('click',()=>{
+                    validatestorageBoolean.forEach(r=>r.style.color='black')
+                })
+                radio.style.color="red"
+               
+            })
+            val_count.push(9)
+            console.log(validatestorageBoolean);
+        }else{
+            storageValue=storageBoolean[0].attributes.id.value
+        }
+       let additionalInfoField=document.getElementById("additional-info")
+       console.log(val_count);
+       ["Cesar Guerrero Garcia", "157  rue Jeanned-d'Arc", "guer0157@algonquinlive.com", "", "consent-no", "", Array(1), "1997-01-15", "", "on-call", "storage-no", ""]
+       if(val_count.length==0){
+        let responsesArray=[nameField.value,addressField.value,emailField.value,phoneField.value,publishBooleanValue,taxEmailField.value,checked_days,startDateField.value,otherHoursField.value,frequencyValue,storageValue,additionalInfoField.value];
+        init.sendToDb(responsesArray)
+    }
+
+   
     },
     sendToDb:function(data){
+        console.log(data)
+
         let fd=new FormData()
         fd.append('name', data[0])
         fd.append('address', data[1])
@@ -191,15 +200,16 @@ let init={
         fd.append('specific-hours', data[8])
         fd.append('pickup-frequency',data[9])
         fd.append('storage', data[10])
-        fd.append('additional-info',data[10])
+        fd.append('additional-info',data[11])
         let opt={
             method:'POST',
             body:fd,
             mode:'cors'
         }
 
-        fetch('http://bottleworks.ca/db/info/postform.php', opt)
-        .then((response)=>{console.log(response.json())})
+        fetch('http://bottleworks.ca/db/api/form.php', opt)
+        .then((response)=>{return response.json()})
+        .then(data=>{console.log(data)})
         .catch(err=>{console.log("Error", err)})
     }
 
