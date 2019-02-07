d069607aab28d3fc141caea80b0c9e7591247c73
guer0157 <32390693+guer0157@users.noreply.github.com>
2019-02-07 13:20:58 -0500

    random
    random

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,4 +1,5 @@
 // http://allengoo.com/php/bottleworksform2.php
+// import svg from '../images/logo-regular.svg'
 let dBdata = {
 init: ()=>{
     fetch('http://bottleworks.ca/db/bottleworksform2.php')
@@ -14,19 +15,57 @@ init: ()=>{
         console.log(data);
         let arrayOfRandom=[]
         // get the amount of items
+        getRandom=(index)=>{
+            let items= data[(Math.floor((Math.random() * data.length) + 0))]
+            arrayOfRandom.push(items)
+        }
         let divs= document.querySelectorAll('.partner-ligroup')
-        divs.forEach(div=>{
-           let items= data[(Math.floor((Math.random() * data.length) + 0))]
-           arrayOfRandom.push(items)
+        divs.forEach((div, index)=>{
+            getRandom(index)
         })
+        console.log(arrayOfRandom)
+        let noDupRandom= arrayOfRandom.filter((object, index, self) =>{
+            console.log("self",self[index])
+            console.log("ob",object)
+            console.log(index)
+            let counter=0;
+        self.findIndex((t) => {
+            console.log("t",t.id);
+            console.log("objId",object.id)
+            if(t.id===object.id){
+                counter++
+            }
+        })
+        if(counter>1){
+            return true
+        }
+    })
+   
+        // if(noDupRandom!=0){
+        //     console.log(noDupRandom.length)
+        //     console.log(arrayOfRandom.findIndex((dup)=>{
+        //         return dup == noDupRandom.id
+        //     })
+        //     )
+        // }
+       
+        
 
         divs.forEach((div, index)=>{
 
             let image=div.childNodes[1].childNodes[1]
             let rest_name=div.childNodes[2].nextElementSibling.children[0]
             let paragraph=div.childNodes[2].nextElementSibling.children[1].children[0]
-            if(!arrayOfRandom[index].image_name==""){ image.src=`http://bottleworks.ca/db/images/${arrayOfRandom[index].image_name}`
-            }
+           
+            // !arrayOfRandom[index].image_name==""
+            if(arrayOfRandom[index].image_name==""){ 
+        
+                image.src="./images/logo-regular.svg"
+
+        }else{
+  
+            image.src=`http://bottleworks.ca/db/images/${arrayOfRandom[index].image_name}`        
+        }
             rest_name.textContent=`${arrayOfRandom[index].restaurant_name}`
             paragraph.textContent=`${arrayOfRandom[index].number_of_bottles}`
         })
